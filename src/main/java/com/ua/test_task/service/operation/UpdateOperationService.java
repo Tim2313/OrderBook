fd8f84137405444bd07e5185e534e7722b4633e1
Tim2313 <120585242+Tim2313@users.noreply.github.com>
2023-12-10 16:01:03 +0200

    [OrderBook] Added maven and logger support

--- src/main/java/com/ua/test_task/service/operation/UpdateOperationService.java
+++ src/main/java/com/ua/test_task/service/operation/UpdateOperationService.java
@@ -3,8 +3,12 @@ package com.ua.test_task.service.operation;
 import com.ua.test_task.model.enums.UpdateOperationType;
 import com.ua.test_task.model.UpdateOperation;
 import com.ua.test_task.service.Memory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UpdateOperationService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UpdateOperationService.class);
 
     private final Memory memory;
 
@@ -20,5 +24,6 @@ public class UpdateOperationService {
         if (updateOperation.getType() == UpdateOperationType.BID) {
             memory.saveBid(updateOperation);
         }
+        LOGGER.info("'UpdateOperation' executed.");
     }
 }
